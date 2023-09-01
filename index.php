34014b146525887fd4f2c61481511d9ff0e1f37c
NushratNowrin <nowrin2023@gmail.com>
2023-09-02 01:33:46 +0600

    banking website done

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<title>Online Banking System</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/chase.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/onbnk2.jpg">
+        <img src="img/onbnk2.jpg">
+        <img src="img/onbnk2.jpg">
+        <img src="img/onbnk2.jpg">
+        <img src="img/onbnk2.jpg">
+        </div>
+    </div><br>
+    <!-- <div class="newsroom">
+        
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed"><span>Your bank may charge you for closing a bank account. 
+        Bank also charges you when you close your account within a particular time period.</span><span>SBI cuts deposit rates; PPF to fetch lower interest rate. </span><span>No, it is not mandatory to link your bank account with CITIZENSHIP card</span></p></marquee>
+    </div><br><br> -->
+   
+    <div class="online_services">
+        <h4>Online Services</h4>
+        <ul>
+            <a href="customer_reg_form.php"><li>Open Account</li></a>
+            <a href="debit_card_form.php"><li>Apply Debit Card</li></a><br>
+            <a href="#" id="ebanking" ><li><div class="ebanking">Internet Banking
+                <div class = "ebanking_options">
+                <ul>
+                    <a href="customer_login.php"><li>Login </li></a>
+                    <a href="ebanking_reg_form.php"><li>Register</li></a>
+                </ul>
+            </div>
+        </div>
+    </li></a>
+            <a href="fund_transfer.php"><li>Fund Transfer</li></a><br>
+        </ul>
+   
+</div>
+
+    
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>
